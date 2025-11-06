11bab91b4277efb86c7c092fccb4030bcbf34d4f
Ghulam Ahmad(haider) <144762176+gahmad92@users.noreply.github.com>
2025-11-07 04:25:47 +0500

    Update TodoItem.jsx
    change color of normal list

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -19,7 +19,7 @@ function TodoItem({ todo }) {
     <>
       <div
         className={`flex border border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300  text-black ${
-          todo.completed ? "accent-indigo-600" : "bg-[#a295ac]"
+          todo.completed ? "accent-indigo-600" : "bg-[#95aca9]"
         }`}
       >
         <input

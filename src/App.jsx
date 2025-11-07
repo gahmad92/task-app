1c3504a7a1b7f40fa34cd1b51164d21d09feb965
Ghulam Ahmad(haider) <144762176+gahmad92@users.noreply.github.com>
2025-11-07 12:00:49 +0500

    Remove import for Tasks page from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
-import Tasks from "./pages/Tasks";
+
 
 import "./App.css";
 import LearnMore from "./pages/LearnMore";

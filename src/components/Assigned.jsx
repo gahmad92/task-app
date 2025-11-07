4c396768027228f75acc88ae4ca9885dd844ab4e
Ghulam Ahmad(haider) <144762176+gahmad92@users.noreply.github.com>
2025-11-07 11:56:30 +0500

    Update Assigned.jsx
    in future

--- src/components/Assigned.jsx
+++ src/components/Assigned.jsx
@@ -1,22 +1,22 @@
-import React from "react";
-import './Assigned.css';
-function Assigned() {
-  return (
-    <>
+// import React from "react";
+// import './Assigned.css';
+// function Assigned() {
+//   return (
+//     <>
 
-    <div className="custom-assigned-box w-64 mx-auto mt-10 p-4 bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 rounded-lg shadow-lg border-2 border-white relative">
-        <h2 className="text-xl font-bold mb-2 text-white text-center">Assigned Tasks</h2>
-      <ol className="text-sm text-neutral-100">her</ol>
-        <ol>her</ol>
-        <ol>her</ol>
-        <ol>her</ol>
+//     <div className="custom-assigned-box w-64 mx-auto mt-10 p-4 bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 rounded-lg shadow-lg border-2 border-white relative">
+//         <h2 className="text-xl font-bold mb-2 text-white text-center">Assigned Tasks</h2>
+//       <ol className="text-sm text-neutral-100">her</ol>
+//         <ol>her</ol>
+//         <ol>her</ol>
+//         <ol>her</ol>
 
-      </div>
+//       </div>
 
 
       
-    </>
-  );
-}
+//     </>
+//   );
+// }
 
-export default Assigned;
+// export default Assigned;
